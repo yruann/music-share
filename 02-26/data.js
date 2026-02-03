cad032c0843f41cd7a6c4505f8ec9125a9f0433d
yruann <yruan@uchicago.edu>
2026-02-03 16:46:38 -0600

    Is This Firefox's Problem Or Did I Mess Up

--- 02-26/data.js
+++ 02-26/data.js
@@ -1,51 +1,234 @@
-function renderList() {
-    const tracklist = [
+let tracklist = [
         {
             num: 1,
-            song: 'song title',
-            artist: 'artist name',
-            album: 'album',
-            year: 1975,
-            duration: '3:25',
-            yt_link: 'https://www.youtube.com/embed/pihwaswzciw',
-            discogs_link: 'https://tidal.com/track/190188581/u',
-            wiki: 'https://en.wikipedia.org/wiki/Lili_Ivanova',
-            notes: '',
-            fav: '',
+            song: `Losing My Edge`,
+            artist: `LCD Soundsystem`,
+            album: `Electricity (A Brief History Of Future Sound)`,
+            year: 2012,
+            duration: `7:53`,
+            yt_link: `https://www.youtube.com/embed/ZUjDMdSwefk`,
+            discogs_release: `3774872`,
+            wiki: ``,
+            notes: `This song is how I feel doing anything and also judging anyone. “I heard you have a compilation of every good song ever done by anybody.” Yeah.`,
+            fav: `true`,
         }, 
-                {
+        {
             num: 2,
-            song: 'song title',
-            artist: 'artist name',
-            album: 'album',
-            country: 'Bulgaria',
-            year: 1975,
-            duration: '3:25',
-            yt_link: 'https://www.youtube.com/embed/pihwaswzciw',
-            discogs_link: 'https://tidal.com/track/190188581/u',
-            wiki: 'https://en.wikipedia.org/wiki/Lili_Ivanova',
-            notes: '',
-            fav: '',
+            song: `Spin Spin Sugar (Armand's Dark Garage Mix)`,
+            artist: `Sneaker Pimps`,
+            album: `MTV Grind Vol. 1`,
+            year: 1997,
+            duration: `9:04`,
+            yt_link: `https://www.youtube.com/embed/lYzOrpWL6U8`,
+            discogs_release: `4088687`,
+            wiki: ``,
+            notes: `This and the next song are both on MTV Grind Vol. 1, which was released for MTV's 'The Grind Workout'. They also had a matching clothing line called "Grind Sport". <a href= 'https://www.youtube.com/watch?v=L6uyNpISHL0' target='_blank'>Here</a> is the full compilation.`,
+            fav: `true`,
         }, 
         {
             num: 3,
-            song: 'song title',
-            artist: 'artist name',
-            album: 'album',
-            country: 'Bulgaria',
-            year: 1975,
-            duration: '3:25',
-            yt_link: 'https://www.youtube.com/embed/pihwaswzciw',
-            discogs_link: 'https://tidal.com/track/190188581/u',
-            wiki: 'https://en.wikipedia.org/wiki/Lili_Ivanova',
-            notes: '',
-            fav: '',
-        }, 
-    ];
+            song: `Sugar Is Sweeter (Armand’s Drum & Bass Mix)`,
+            artist: `CJ Bolland`,
+            album: `MTV Grind Vol. 1`,
+            year: 1997,
+            duration: `8:36`,
+            yt_link: ``,
+            discogs_release: `4088687`,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+        {
+            num: 4,
+            song: `45:33 (Pilooski Remix)`,
+            artist: `LCD Soundsystem`,
+            album: `The Man Machine (Mojo celebrates the electronic revolution)`,
+            year: 2009,
+            duration: `8:36`,
+            yt_link: ``,
+            discogs_release: `1952980`,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+        {
+            num: 5,
+            song: "Insect Near Piha Beach",
+            artist: `Four Tet`,
+            album: `Sixteen Oceans`,
+            year: 2020,
+            duration: `5:16`,
+            yt_link: ``,
+            discogs_release: `15565519`,
+            wiki: ``,
+            notes: `I found this album for 99 cents in the suburbs. Still don’t know how that happened.`,
+            fav: ``,
+        },
+        {
+            num: 6,
+            song: `Subduction (Continental Version)`,
+            artist: `Hanssen, Piper Davis`,
+            album: `Transit`,
+            year: 2016,
+            duration: `5:59`,
+            yt_link: ``,
+            discogs_release: `27020385`,
+            wiki: ``,
+            notes: `This is the cassette track. The artist is quite unknown, but this is definitely one of my favorite tracks in here… Coconut Palm Forest from this album is also amazing.`,
+            fav: ``,
+        },
+        {
+            num: 7,
+            song: `Human Behavior`,
+            artist: `Björk`,
+            album: `Debut`,
+            year: 1993,
+            duration: `4:12`,
+            yt_link: ``,
+            discogs_release: `2431`,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+        {
+            num: 8,
+            song: `Free to Choose, Hard to Be`,
+            artist: `Statik Sound System`,
+            album: `Temptesta II`,
+            year: 1997,
+            duration: `4:29`,
+            yt_link: ``,
+            discogs_release: `28329`,
+            wiki: ``,
+            notes: ``,
+            fav: `true`,
+        },
+        {
+            num: 9,
+            song: `If I Was Your Girlfriend`,
+            artist: `TLC`,
+            album: `CrazySexyCool`,
+            year: 1994,
+            duration: `4:37`,
+            yt_link: ``,
+            discogs_release: `8950062`,
+            wiki: ``,
+            notes: `I had to include TLC on here somehow. This playlist started out very TLC heavy, but as I worked on it, I realized I had forgotten about many of the electronic CDs I bought in the summer. This isn’t my favorite TLC song, but the intro scratches a particular itch and also fits well here. I love the intro. I didn’t know this was a cover of Prince since I didn’t grow up listening to anything other than 2010s pop and country radio in the car 💔 This cover is distinctly 90s girl group, but the original has so much more sensuality and depth (go listen to Prince).`,
+            fav: ``,
+        },
+        {
+            num: 10,
+            song: "Be No One",
+            artist: `Charles Webster`,
+            album: `Back to Mine: The Orb`,
+            year: 2003,
+            duration: `5:26`,
+            yt_link: ``,
+            discogs_release: `565185`,
+            wiki: ``,
+            notes: `This compilation CD from The Orb is one of the first CDs I bought myself. The Twin Peaks theme is on here and so is a convoluted version of the Barbie song? Another classic is "I Wanna Be a Fishy", which goes, “I wanna be a little fishy, yes I do, yes I do, yes I dooo. I wanna swim around and be a little fish clown cause it is so cool to be under the waterrrr” (actually a great song) <br> Also look at the <a href="https://www.discogs.com/release/565185-The-Orb-Back-To-Mine/image/SW1hZ2U6NTE4MjQ5Ng==">track commentary</a> on this thing. "Would you let your daughter near this man?" (about Aphex Twin)`,
+            fav: ``,
+        },
+        {
+            num: 11,
+            song: `Samba Tranquille`,
+            artist: `Thievery Corporation`,
+            album: `The Mirror Conspiracy`,
+            year: 2000,
+            duration: `3:04`,
+            yt_link: ``,
+            discogs_release: `3477`,
+            wiki: ``,
+            notes: `Maybe the real mirror conspiracy is the fact that this reflective disc looks the same on the front and back....`,
+            fav: ``,
+        },
+        {
+            num: 12,
+            song: `Maria Moita`,
+            artist: `Rosalia de Souza`,
+            album: `Garota Moderna`,
+            year: 2002,
+            duration: `5:16`,
+            yt_link: ``,
+            discogs_release: `2039308`,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+        {
+            num: 5,
+            song: ``,
+            artist: ``,
+            album: ``,
+            year: 200,
+            duration: ``,
+            yt_link: ``,
+            discogs_release: ``,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+        {
+            num: 5,
+            song: ``,
+            artist: ``,
+            album: ``,
+            year: 200,
+            duration: ``,
+            yt_link: ``,
+            discogs_release: ``,
+            wiki: ``,
+            notes: ``,
+            fav: ``,
+        },
+];
 
 
-    const template = '{{#tracklist}} <div class="track-container {{#fav}}fav-track{{/fav}} "> <button type="button" class="coll-closed track-toggle"> <h1 class="track-num">{{num}}</h1> <img class="album-icon" src="{{num}}.png"> <div class="song-info"> <h3>{{song}}</h3> <p>{{artist}}</p> </div> <p class="album">{{album}}</p> <p class="duration">{{duration}}</p> </button> <div class="content_closed"> <div class="media-container"> <table cellspacing="0"> <tr> <th>Song</th> <td>{{song}}</td> </tr> <tr> <th>Artist</th> <td>{{artist}}</td> </tr> <tr> <th>Album</th> <td>{{album}}</td> </tr> <tr> <th>Year</th> <td>{{year}}</td> </tr></table><iframe class="vid" src="{{yt_link}}?autoplay=0" title="YouTube video player" frameborder="0" modestbranding allowfullscreen></iframe> </div> {{#notes}}<div class="media-container"> <h3>Notes:</h3> <p>{{{.}}}</p> </div>{{/notes}} <div class="music-link-container"> {{#discogs_link}}<div class="music-link"> <img src="../img/icons/discogs.png"> <a href="{{.}}" target="_blank"><p>album discogs</p></a> </div> {{/discogs_link}}{{#wiki}}<div class="music-link"> <img src="../img/icons/wiki.png"> <a href="{{.}}" target="_blank"><p>artist wiki</p></a> </div> {{/wiki}}</div> </div> </div> {{/tracklist}}';
-    const rendered = Mustache.render(template, {tracklist});
+tracklist.forEach((track, index) => {
+    track.originalNum = index + 1;
+    track.discogs_release = getDiscogsLink(track.discogs_release);
+});
+
+function getDiscogsLink(release) {
+    if (!release) return '';
+    const link = 'https://www.discogs.com/release/' + release;
+    return link;
+}
+
+function renderList(data) {    
+        const template = '{{#tracklist}} <div class="track-container {{#fav}}fav-track{{/fav}} "> <button type="button" class="coll-closed track-toggle"> <h1 class="track-num">{{num}}</h1> <img class="album-icon" src="{{num}}.png"> <div class="song-info"> <h3>{{song}}</h3> <p>{{artist}}</p> </div> <p class="album">{{album}}</p> <p class="duration">{{duration}}</p> </button> <div class="content_closed"> <div class="media-container"> <table cellspacing="0"> <tr> <th>Song</th> <td>{{song}}</td> </tr> <tr> <th>Artist</th> <td>{{artist}}</td> </tr> <tr> <th>Album</th> <td>{{album}}</td> </tr> <tr> <th>Year</th> <td>{{year}}</td> </tr></table><iframe class="vid" src="{{yt_link}}?autoplay=0" title="YouTube video player" frameborder="0" modestbranding allowfullscreen></iframe> </div> {{#notes}}<div class="media-container"> <h3>Notes:</h3> <p>{{{.}}}</p> </div>{{/notes}} <div class="music-link-container"> {{#discogs_release}}<div class="music-link"> <img src="../img/icons/discogs.png"> <a href="{{.}}" target="_blank"><p>album discogs</p></a> </div> {{/discogs_release}}{{#wiki}}<div class="music-link"> <img src="../img/icons/wiki.png"> <a href="{{.}}" target="_blank"><p>artist wiki</p></a> </div> {{/wiki}}</div> </div> </div> {{/tracklist}}';
+
+    const rendered = Mustache.render(template, {tracklist: data});
     document.getElementById('tracklist-placeholder').innerHTML = rendered;
     initTracklistInteractions();
 }
+
+function setActiveSortButton(activeClass) {
+    const buttons = document.querySelectorAll('.sort-btn');
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+    const activeBtn = document.querySelector(`.sort-btn.${activeClass}`);
+    if (activeBtn) activeBtn.classList.add('active');
+}
+
+function sortByDefault() {
+    const sorted = [...tracklist].sort((a, b) => a.originalNum - b.originalNum);
+    sorted.forEach((track, index) => {
+    track.num = index + 1;
+    });
+    renderList(sorted);
+    setActiveSortButton('sort-default');
+}
+function sortByYear() {
+    const sorted = [...tracklist].sort((a, b) => (a.year || 0) - (b.year || 0));
+    sorted.forEach((track, index) => {
+    track.num = index + 1;
+    });
+
+    renderList(sorted);
+    setActiveSortButton('sort-year');
+}
+
+
+renderList(tracklist);
+    
